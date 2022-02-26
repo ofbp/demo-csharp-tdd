108c6e8c642f09a9bd28e7a2cebf394afeec1e68
dtc3415 <dtc3415@autuni.ac.nz>
2022-02-26 14:32:38 +1300

    json version

---
+++ BitcoinConverter.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using CloudAcademy.BitcoinCoverter.Code;
+
+namespace BitcoinConverter.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var converter = new ConverterSvc();
+
+            while(true)
+            {
+                try
+                {
+                    Console.Write("Coins: ");
+                    var coins = Double.Parse(Console.ReadLine());
+                    Console.Write("Currency (USD, GBP, or EUR): ");
+                    var currency = (Currency)Enum.Parse(typeof(Currency), Console.ReadLine(), true);
+
+                    var amount = await converter.ConvertBitcoins(currency, coins);
+
+                    Console.WriteLine("{0} Bitcoins = {1} {2}", coins, amount, currency);
+                }
+                catch 
+                {
+                    //swallow
+                }
+            }
+        }
+    }
+}
